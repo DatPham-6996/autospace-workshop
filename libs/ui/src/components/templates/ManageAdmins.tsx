8e13878c5b5b9944f87d52114b34aa0299387f2d
Dat Pham <dat.pham_ext@acciona.com>
2025-02-26 16:45:53 +0700

    test

--- libs/ui/src/components/templates/ManageAdmins.tsx
+++ libs/ui/src/components/templates/ManageAdmins.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { useTakeSkip } from '@autospace/util/hooks/pagination'
-import { useMutation, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { AdminsDocument } from '@autospace/network/src/gql/generated'
 import { ShowData } from '../organisms/ShowData'
 import { AdminCard } from '../organisms/admin/AdminCard'
 import { RemoveAdminButton } from '../organisms/admin/RemoveAdminButton'
 import { CreateAdmin } from '../organisms/admin/CreateAdmin'
 
-export const ManageAdmins = () => {
+export function ManageAdmins() {
   const { setSkip, setTake, skip, take } = useTakeSkip(0)
 
   const { data, loading } = useQuery(AdminsDocument, {
@@ -15,32 +15,53 @@ export const ManageAdmins = () => {
   })
 
   return (
-    <>
-      <div className="flex justify-end">
-        <CreateAdmin />
+    <div className="relative min-h-screen">
+      {/* Main background */}
+      <div className="absolute inset-0 z-0">
+        <img
+          src="/Im-2.svg"
+          alt="Background"
+          className="w-full h-full object-cover opacity-10"
+        />
       </div>
-      <ShowData
-        loading={loading}
-        pagination={{
-          skip,
-          take,
-          resultCount: data?.admins.length,
-          totalCount: data?.adminsCount,
-          setSkip,
-          setTake,
-        }}
-        title={'Manage admins'}
-      >
-        {data?.admins.map((admin) => (
-          <div key={admin.uid} className="pl-0.5 border-l-2  border-primary">
-            <AdminCard key={admin.uid} admin={admin}>
-              <div className="flex justify-end">
-                <RemoveAdminButton uid={admin.uid} />
+
+      {/* Content */}
+      <div className="relative z-10">
+        <div className="flex justify-end">
+          <CreateAdmin />
+        </div>
+        <ShowData
+          loading={loading}
+          pagination={{
+            skip,
+            take,
+            resultCount: data?.admins.length,
+            totalCount: data?.adminsCount,
+            setSkip,
+            setTake,
+          }}
+          title={'Manage admins'}
+        >
+          {data?.admins.map((admin) => (
+            <div key={admin.uid} className="pl-0.5 border-l-2 border-primary">
+              <div className="flex items-center gap-4 bg-white rounded-lg p-4 mb-4">
+                <img
+                  src="/Logo.svg"
+                  alt="Logo"
+                  className="w-16 h-16"
+                />
+                <div className="flex-1">
+                  <AdminCard admin={admin}>
+                    <div className="flex justify-end">
+                      <RemoveAdminButton uid={admin.uid} />
+                    </div>
+                  </AdminCard>
+                </div>
               </div>
-            </AdminCard>
-          </div>
-        ))}
-      </ShowData>
-    </>
+            </div>
+          ))}
+        </ShowData>
+      </div>
+    </div>
   )
 }
